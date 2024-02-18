b679ea2b131e2519387e194275e355d15792f624
RohanShrestha98 <nahorshrestha@gmail.com>
2024-02-18 16:04:51 +0545

    responsive

--- src/pages/team/TechnicalTeam.jsx
+++ src/pages/team/TechnicalTeam.jsx
@@ -10,6 +10,7 @@ import teammember7 from "../../assets/teammember/teammember7.svg";
 import teammember8 from "../../assets/teammember/teammember8.svg";
 import teammember9 from "../../assets/teammember/teammember9.svg";
 import teammember10 from "../../assets/teammember/teammember10.svg";
+import TeamMember from "../../components/TeamMember";
 
 export default function TechnicalTeam() {
   const [selectedId, setSelectedId] = useState(1);
@@ -128,28 +129,7 @@ export default function TechnicalTeam() {
           );
         })}
       </div>
-      <div className="grid grid-cols-5 md:grid-cols-3 sm:grid-cols-2 gap-6 md:gap-4 sm:gap-2">
-        {teamImages?.map((team) => {
-          return (
-            <div
-              key={team?.id}
-              className="flex flex-col items-center mb-4 sm:mb-2"
-            >
-              <img
-                src={team?.img}
-                className="w-full h-full object-cover rounded-lg"
-                alt=""
-              />
-              <h1 className="mt-2 uppercase text-[#5E5555] text-[13px] font-medium">
-                {team?.name}
-              </h1>
-              <h1 className="text-[#999999] text-[13px] ">
-                {team?.positionName}
-              </h1>
-            </div>
-          );
-        })}
-      </div>
+      <TeamMember data={teamImages} />
     </div>
   );
 }
